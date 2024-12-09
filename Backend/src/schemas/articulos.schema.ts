47f2766a83e4e6129b57cbffef5ea1260be4449f
jacq1813 <l21170454@culiacan.tecnm.mx>
2024-12-08 18:48:18 -0700

    fecha

--- Backend/src/schemas/articulos.schema.ts
+++ Backend/src/schemas/articulos.schema.ts
@@ -5,6 +5,6 @@ export const articulosSchema = z.object({
     descripcion: z.string().min(2, "Minimo 2 caracteres").max(300, "Maximo 300 caracteres"),
     precio: z.number().positive().min(1),
     cantidad_almacen: z.number().int().positive(),
-    fecha_caducidad: z.date().min(new Date())
+    fecha_caducidad: z.date()
 
     });
