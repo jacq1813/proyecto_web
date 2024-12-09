885bb0cf68d6fc9c2c465e7ea95f857a192c8d49
jacq1813 <l21170454@culiacan.tecnm.mx>
2024-12-08 19:37:27 -0700

    otro

--- Backend/src/schemas/articulos.schema.ts
+++ Backend/src/schemas/articulos.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const articulosSchema = z.object({
-
+    id: z.number().int().positive(),
     descripcion: z.string().min(2, "Minimo 2 caracteres").max(300, "Maximo 300 caracteres"),
     precio: z.number().positive().min(1),
     cantidad_almacen: z.number().int().positive(),
