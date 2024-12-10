749c4bc664294ba3413eec9089a75bd62fa235f4
jacq1813 <l21170454@culiacan.tecnm.mx>
2024-12-09 19:09:14 -0700

    articulos form editar

--- cliente_tienda/src/modulos/articulos/vistas/ArticulosEditarVue.vue
+++ cliente_tienda/src/modulos/articulos/vistas/ArticulosEditarVue.vue
@@ -2,57 +2,73 @@
     <div class="container mt-5" v-if="articulos[0]">
         <div class="card">
             <div class="card-header">
-                <h4>Editar articulo</h4>
+                <h4>Editar artículo</h4>
             </div>
-            <div v-if="mensaje == 1" class="alert alert-succes" role="alert">
+            <div v-if="mensaje === 1" class="alert alert-success" role="alert">
                 Datos actualizados con éxito
             </div>
             <div class="card-body">
+                <Form :validation-schema="ArticulosSchema" @submit="onTodoBien">
                     <div class="mb-3">
                         Id
-                        <input type="text" class="form-control" v-model="articulos[0].id" disabled>
+                        <Field name="id" type="text" class="form-control" :value="articulos[0].id" disabled />
                     </div>
                     <div class="mb-3">
                         Descripción
-                        <input type="text" class="form-control" v-model="articulos[0].descripcion">
+                        <Field name="descripcion" type="text" class="form-control" v-model="articulos[0].descripcion" />
+                        <ErrorMessage name="descripcion" class="errorValidacion" />
                     </div>
                     <div class="mb-3">
                         Precio
-                        <input type="text" class="form-control" v-model="articulos[0].precio">
+                        <Field name="precio" type="number" class="form-control" v-model="articulos[0].precio" />
+                        <ErrorMessage name="precio" class="errorValidacion" />
                     </div>
                     <div class="mb-3">
-                        Cantidad en almacen
-                        <input type="text" class="form-control" v-model="articulos[0].cantidad_almacen">
+                        Cantidad en almacén
+                        <Field name="cantidad_almacen" type="number" class="form-control"
+                            v-model="articulos[0].cantidad_almacen" />
+                        <ErrorMessage name="cantidad_almacen" class="errorValidacion" />
                     </div>
                     <div class="mb-3">
                         Fecha de caducidad
-                        <input type="date" class="form-control" v-model="articulos[0].fecha_caducidad">
+                        <Field name="fecha_caducidad" type="date" class="form-control"
+                            v-model="articulos[0].fecha_caducidad" />
+                        <ErrorMessage name="fecha_caducidad" class="errorValidacion" />
                     </div>
                     <div class="mb-3">
-                        <button class="btn btn-primary" @click="actualizarArticulo(articulos[0])">Actualizar</button>
+                        <button class="btn btn-primary" type="submit">Actualizar</button>
                     </div>
-                </div>
+                </Form>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ErrorMessage, Field, Form } from 'vee-validate';
 import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import { useArticulos } from '../controladores/useArticulos';
-const { traeArticulosId,mensaje,articulos,actualizarArticulo } = useArticulos();
-
 import { ArticulosSchema } from '../schemas/ArticulosSchema';
-import {Field,Form,ErrorMessage} from 'vee-validate';
+
+const { traeArticulosId, mensaje, articulos, actualizarArticulo } = useArticulos();
 
 let IdArticulo = 0;
 const route = useRoute();
-onMounted(async() => {
+
+onMounted(async () => {
     IdArticulo = Number(route.params.id);
     await traeArticulosId(IdArticulo);
-})
+});
+
+const onTodoBien = async () => {
+    await actualizarArticulo(articulos.value[0]);
+};
 </script>
 
 <style scoped>
-
+.errorValidacion {
+    color: red;
+    font-weight: bold;
+}
 </style>
